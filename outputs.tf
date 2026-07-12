21e4c66efa0cb9f68abace696deda3e18c0e5322
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "load_tests" {
-  description = "All load_test resources"
-  value       = azurerm_load_test.load_tests
-}
 output "load_tests_data_plane_uri" {
-  description = "List of data_plane_uri values across all load_tests"
-  value       = [for k, v in azurerm_load_test.load_tests : v.data_plane_uri]
+  description = "Map of data_plane_uri values across all load_tests, keyed the same as var.load_tests"
+  value       = { for k, v in azurerm_load_test.load_tests : k => v.data_plane_uri }
 }
 output "load_tests_description" {
-  description = "List of description values across all load_tests"
-  value       = [for k, v in azurerm_load_test.load_tests : v.description]
+  description = "Map of description values across all load_tests, keyed the same as var.load_tests"
+  value       = { for k, v in azurerm_load_test.load_tests : k => v.description }
 }
 output "load_tests_encryption" {
-  description = "List of encryption values across all load_tests"
-  value       = [for k, v in azurerm_load_test.load_tests : v.encryption]
+  description = "Map of encryption values across all load_tests, keyed the same as var.load_tests"
+  value       = { for k, v in azurerm_load_test.load_tests : k => v.encryption }
 }
 output "load_tests_identity" {
-  description = "List of identity values across all load_tests"
-  value       = [for k, v in azurerm_load_test.load_tests : v.identity]
+  description = "Map of identity values across all load_tests, keyed the same as var.load_tests"
+  value       = { for k, v in azurerm_load_test.load_tests : k => v.identity }
 }
 output "load_tests_location" {
-  description = "List of location values across all load_tests"
-  value       = [for k, v in azurerm_load_test.load_tests : v.location]
+  description = "Map of location values across all load_tests, keyed the same as var.load_tests"
+  value       = { for k, v in azurerm_load_test.load_tests : k => v.location }
 }
 output "load_tests_name" {
-  description = "List of name values across all load_tests"
-  value       = [for k, v in azurerm_load_test.load_tests : v.name]
+  description = "Map of name values across all load_tests, keyed the same as var.load_tests"
+  value       = { for k, v in azurerm_load_test.load_tests : k => v.name }
 }
 output "load_tests_resource_group_name" {
-  description = "List of resource_group_name values across all load_tests"
-  value       = [for k, v in azurerm_load_test.load_tests : v.resource_group_name]
+  description = "Map of resource_group_name values across all load_tests, keyed the same as var.load_tests"
+  value       = { for k, v in azurerm_load_test.load_tests : k => v.resource_group_name }
 }
 output "load_tests_tags" {
-  description = "List of tags values across all load_tests"
-  value       = [for k, v in azurerm_load_test.load_tests : v.tags]
+  description = "Map of tags values across all load_tests, keyed the same as var.load_tests"
+  value       = { for k, v in azurerm_load_test.load_tests : k => v.tags }
 }
 
